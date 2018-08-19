74d6bb265b57a11901f944b738573c85bee094ed
David McGrann <dpmcgrann@gmail.com>
2018-08-19 18:25:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ class App < Sinatra::Base
   end
 
   get '/first_exercise' do
+    session[:foo] = "hello"
     "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, navigate to the '/set' path."
   end
 
